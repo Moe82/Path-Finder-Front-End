a9f03a21c865a07ab9fc90482c6ff1631d1649d0
Mohamed Shafee <mshafee1993@gmail.com>
2021-04-12 10:45:11 -0400

    change node color on drag over

--- path-finder-front-end/src/components/views/Node.js
+++ path-finder-front-end/src/components/views/Node.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/Node.css';
 
-function Node() {
-	return <div className={'node'}></div>;
+function Node(props) {
+	const [backgroundColor, setColor] = useState('#272822');
+	const [isHighlited, setHighlited] = useState(false);
+	return (
+		<div
+			draggable="true"
+			className={'node'}
+			style={{
+				backgroundColor: backgroundColor,
+				width: '2%',
+				paddingBottom: '2%',
+			}}
+			onDragOver={(e) => {
+				setColor('#FFFFFF');
+				setHighlited(true);
+			}}
+		></div>
+	);
 }
 
 export default Node;
