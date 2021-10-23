36039fbca790979420f1404d60329835243d3487
Moe82 <mshafee1993@gmail.com>
2021-10-23 17:31:40 -0400

    remove unneeded exports

--- path-finder-front-end/src/components/views/index.js
+++ path-finder-front-end/src/components/views/index.js
@@ -1,4 +1,2 @@
-export { default as EndNode } from './EndNode.js';
-export { default as StartNode } from './StartNode.js';
 export { default as Node } from './Node.js';
 export { default as GridView } from './GridView.js';
