d6be2060e029e4103e5f627749f0508dc3dce699
Moe82 <mshafee1993@gmail.com>
2021-10-23 17:13:16 -0400

    allow start/end nodes to be repositioned

--- path-finder-front-end/src/components/views/Node.js
+++ path-finder-front-end/src/components/views/Node.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/Node.css';
 
 function Node(props) {
-  const nodeTypeToColorMap = {
+  const nodeIDToColorMap = {
     0: '#FFFFFF', // free path
     1: '#A9DC76', // start node
     2: '#FF6188', // end node
@@ -11,34 +11,72 @@ function Node(props) {
     5: '#FF0000', // node that is part of shortest path
   };
 
-  const [backgroundColor, setColor] = useState(nodeTypeToColorMap[props.type]);
+  const nodeTypeToIDMap = {
+    'free path': 0,
+    start: 1,
+    end: 2,
+    wall: 3,
+  };
+
   let map = props.map;
+  const [draggingNodeFlag, setDraggingNodeFlag] = useState(
+    map[map.length - 1][map[map[0].length - 1]]
+  );
+  const [backgroundColor, setColor] = useState(nodeIDToColorMap[props.type]);
+
   return (
     <div
       draggable="true"
       className={'node'}
       style={{
         backgroundColor:
-          props.type !== 0 ? nodeTypeToColorMap[props.type] : backgroundColor,
+          props.type !== 0 ? nodeIDToColorMap[props.type] : backgroundColor,
         width: '2%',
         paddingBottom: '2%',
       }}
-      onDragOver={(e) => {
-        let row = props.x;
-        let col = props.y;
-        if (
-          noSurroundingWalls(row, col, map) ||
-          (row > 0 && map[row - 1][col] === 3) ||
-          (row < map.length - 1 && map[row + 1][col] === 3) ||
-          (col > 0 && map[row][col - 1] === 3) ||
-          (col < map[0].length - 1 && map[row][col + 1] === 3)
-        ) {
-          map[props.x][props.y] = 3;
-          setColor(nodeTypeToColorMap[3]);
+      onDragStart={(e) => {
+        if (props.type === nodeTypeToIDMap['end']) {
+          map[props.x][props.y] = nodeTypeToIDMap['free path'];
+          setColor(nodeIDToColorMap[0]);
+          props.updateNodes(map);
+          props.setRepositioningEndNode(true);
+        } else if (props.type === nodeTypeToIDMap['start']) {
+          map[props.x][props.y] = nodeTypeToIDMap['free path'];
+          setColor(nodeIDToColorMap[0]);
+          props.updateNodes(map);
+          props.setRepositioningStartNode(true);
         }
       }}
-      onDragEnd={(e) => {
-        props.updateNodes(map, 3);
+      onDragOver={(e) => {
+        e.preventDefault();
+        if (!props.repositioningEndNode && !props.repositioningStartNode) {
+          let row = props.x;
+          let col = props.y;
+          if (
+            noSurroundingWalls(row, col, map) ||
+            (row > 0 && map[row - 1][col] === 3) ||
+            (row < map.length - 1 && map[row + 1][col] === 3) ||
+            (col > 0 && map[row][col - 1] === 3) ||
+            (col < map[0].length - 1 && map[row][col + 1] === 3)
+          ) {
+            map[props.x][props.y] = 3;
+            setColor(nodeIDToColorMap[3]);
+          }
+        }
+      }}
+      onDrop={(e) => {
+        if (props.repositioningEndNode) {
+          map[props.x][props.y] = 2;
+          setColor(nodeIDToColorMap[2]);
+          props.updateNodes(map);
+          props.setRepositioningEndNode(false);
+        } else if (props.repositioningStartNode) {
+          map[props.x][props.y] = 1;
+          setColor(nodeIDToColorMap[1]);
+          props.updateNodes(map);
+          props.setRepositioningStartNode(false);
+        }
+        if (props.type) props.updateNodes(map);
       }}
     />
   );
